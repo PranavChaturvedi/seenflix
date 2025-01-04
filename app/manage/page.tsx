afcc0e576c521a74b2b8c41972b2246fd5d0dd89
Pranav Chaturvedi <pranavhfs1@gmail.com>
2025-01-04 20:03:57 +0530

    Clerk Implemented. Start Backend Changes

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -8,10 +8,11 @@ export default function GetStarted() {
     <main className="flex min-h-screen flex-col bg-gray-900 text-white">
       <Header />
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-purple-400 mb-8 text-center">Get Started with SeenFlix</h1>
+        <h1 className="text-4xl font-bold text-purple-400 mb-8 text-center">Manage Your Watchlist</h1>
         <div className="flex justify-center mb-8">
           <SearchInput />
         </div>
+        <h4 className="text-4xl font-bold text-purple-400 mb-8 text-left ml-5">Recommendations</h4>
         <RecommendationCards />
       </div>
     </main>
