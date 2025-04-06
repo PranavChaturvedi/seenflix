41d3327aa227e9c4c4593aee43af69a6b6b64731
Pranav Chaturvedi <pranavhfs1@gmail.com>
2025-04-06 11:23:20 +0530

    tweak

--- app/common/jwtToken.tsx
+++ app/common/jwtToken.tsx
@@ -1,4 +1,5 @@
-export async function getToken(session: any) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function getToken(session : any) {
   const token = await session?.getToken({
     template: process.env.NEXT_PUBLIC_CLERK_JWT_TEMPLATE,
   });
